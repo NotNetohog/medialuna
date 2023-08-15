d08636334ddfab8bb02e4ce0d1e5b8d3a9ae1556
Neto Hog <neto@bornlogic.com>
2023-08-15 14:44:07 -0300

    remove old favicon

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,4 @@ import { dev } from "$app/environment"
 
 export const title = 'medialuna 🥐'
 export const description = 'para acompañar el desayuno o la merienda'
-export const url = dev ? 'http://localhost:5173/' : 'url'
+export const url = dev ? 'http://localhost:5173/' : 'https://medialuna.tech'
