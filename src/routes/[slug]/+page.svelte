39de8f822b27403dedfa6b3fbb13fa056a8b83e0
Neto Hog <neto@bornlogic.com>
2023-08-15 18:37:36 -0300

    cafecito

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -24,6 +24,15 @@
 	<div class="prose">
 		<svelte:component this={data.content} />
 	</div>
+	<div class="cafecito">
+		<a href="https://cafecito.app/netohog" rel="noopener" target="_blank"
+			><img
+				srcset="https://cdn.cafecito.app/imgs/buttons/button_6.png 1x, https://cdn.cafecito.app/imgs/buttons/button_6_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_6_3.75x.png 3.75x"
+				src="https://cdn.cafecito.app/imgs/buttons/button_6.png"
+				alt="Invitame un café en cafecito.app"
+			/></a
+		>
+	</div>
 </article>
 
 <style>
@@ -34,6 +43,9 @@
 	}
 
 	hgroup {
+		& h1 {
+			font-size: var(--font-size-fluid-3);
+		}
 		& p {
 			color: var(--text-2);
 		}
@@ -50,5 +62,10 @@
 			background-color: var(--brand);
 			color: var(--text-1);
 		}
+	}
+
+	.cafecito {
+		margin-block-start: var(--size-5);
+		inline-size: var(--size-12);
 	}
 </style>
